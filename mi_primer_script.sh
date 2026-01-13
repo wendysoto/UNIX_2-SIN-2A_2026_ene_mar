d37944fb5b98acd9319ec0c408b67bb4eb5a2923
wendysoto <38251149+wendysoto@users.noreply.github.com>
2026-01-13 02:06:26 +0000

    comandos basicos de linux

---
+++ mi_primer_script.sh
@@ -0,0 +1,21 @@
+#cambia de directorio
+cd / 
+#lista directorio actual
+ls
+#cambia a directorio dev
+cd dev
+ls
+#cambio a directorio bin
+cd /bin
+ls
+#ver contenido del archivo
+cat ls
+file ls
+# punto  es directorio actual
+# dos puntos es directorio padre
+cd ..
+#/etc almacena las configuraciones
+cd /etc
+ls
+#Imprime variable de entorno SHELL (el que interpreta los comandos)
+echo $SHELL
