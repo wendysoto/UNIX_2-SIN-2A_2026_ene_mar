5ec0d801bfc5abf5e3f3a45dcc3a0999cc97e4e7
wendysoto <38251149+wendysoto@users.noreply.github.com>
2026-01-20 02:03:34 +0000

    wip: cambios

--- semana3/script_s3.sh
+++ semana3/script_s3.sh
@@ -1,3 +1,16 @@
 ls -l /home
 touch 'script s3'
 cat script\ s3 
+echo Hola UNIX
+printf "Hola UNIX\n"
+echo Hola UNIX1
+echo Hola UNIX2
+#print working directory
+pwd
+printf "Hola %s\n" "UNIX"
+ps -a -u -x
+ps -aux
+mkdir test
+ls
+rmdir test
+ls 
