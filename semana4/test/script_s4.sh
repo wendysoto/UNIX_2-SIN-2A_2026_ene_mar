d9fdbf6f10ce14fa56752eee9b76ec2f9d712e9f
wendysoto <38251149+wendysoto@users.noreply.github.com>
2026-01-27 01:56:56 +0000

    wip: cp & mv

---
+++ semana4/test/script_s4.sh
@@ -0,0 +1,10 @@
+mkdir -p padre_ws/hijo_ws
+pwd
+cd
+pwd
+cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_ws/hijo_ws
+touch archivo_ws.txt
+echo '0' > archivo_ws.txt
+echo '1' >> archivo_ws.txt
+echo '2' >> archivo_ws.txt
+echo '3' >> archivo_ws.txt
